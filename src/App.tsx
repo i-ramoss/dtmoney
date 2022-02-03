d26370c1f3e77de2849628d8e2157e9820a31dce
Ian Ramos <ianramossantos@hotmail.com>
2022-02-03 19:11:30 -0300

    feat: create new transaction modal component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from "react-modal";
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
 
@@ -25,12 +26,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal 
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Register transaction</h2>
-        </Modal>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
         
       <GlobalStyle />
     </>
