32b6460fbb796902fb55aa3dfabd61ff555d5efd
Ian Ramos <ianramossantos@hotmail.com>
2022-02-03 06:57:06 -0300

    style: create global style for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
+      
+      <GlobalStyle />
     </div>
   );
 }
