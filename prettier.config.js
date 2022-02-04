5d7512166924a12a2a7528209a2ecddbcd432f98
Ian Ramos <ianramossantos@hotmail.com>
2022-02-04 07:44:28 -0300

    chore: setting up eslint and prettier on app

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  trailingComment: 'all',
+  allowParens: 'avoid',
+}
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,27 +1,33 @@
-import { FormEvent, useState } from "react";
-import Modal from "react-modal";
+import { FormEvent, useState } from 'react';
+import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 
-import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
-export function NewTransactionModal ({isOpen, onRequestClose}: NewTransactionModalProps) {
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
   const [title, setTitle] = useState('');
   const [value, setValue] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction (event: FormEvent) {
+  function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({ title, value, category, type });
+    const data = { title, value, category, type };
+
+    api.post('/transactions', data);
   }
 
   return (
@@ -45,14 +51,14 @@ export function NewTransactionModal ({isOpen, onRequestClose}: NewTransactionMod
         <input
           placeholder="Title"
           value={title}
-          onChange={event => setTitle(event.target.value)}
+          onChange={(event) => setTitle(event.target.value)}
         />
 
         <input
           type="number"
           placeholder="Value"
           value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          onChange={(event) => setValue(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
@@ -60,7 +66,7 @@ export function NewTransactionModal ({isOpen, onRequestClose}: NewTransactionMod
             type="button"
             onClick={() => setType('deposit')}
             isActive={type === 'deposit'}
-            activeColor='green'
+            activeColor="green"
           >
             <img src={incomeImg} alt="Income value" />
             <span>Income</span>
@@ -70,7 +76,7 @@ export function NewTransactionModal ({isOpen, onRequestClose}: NewTransactionMod
             type="button"
             onClick={() => setType('withdraw')}
             isActive={type === 'withdraw'}
-            activeColor='red'
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Outcome value" />
             <span>Outcome</span>
@@ -80,7 +86,7 @@ export function NewTransactionModal ({isOpen, onRequestClose}: NewTransactionMod
         <input
           placeholder="Category"
           value={category}
-          onChange={event => setCategory(event.target.value)}
+          onChange={(event) => setCategory(event.target.value)}
         />
 
         <button type="submit">Register</button>
