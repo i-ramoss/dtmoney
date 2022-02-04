449ef580ad36c16b91d3393cf81d5065a03c9b16
Ian Ramos <ianramossantos@hotmail.com>
2022-02-04 19:39:06 -0300

    feat: finish transaction insertion on mirage api

--- src/TransactionsContexts.tsx
+++ src/TransactionsContexts.tsx
@@ -18,9 +18,8 @@ interface TransactionsProviderProps {
 }
 
 interface TransactionsContextData {
-  // informa qual é o conteúdo do contexto.
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -36,8 +35,15 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions));
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transactions', transaction);
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
   }
 
   return (
